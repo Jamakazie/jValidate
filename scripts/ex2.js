416cdebf178bf0f6e668c0e7c316131dbf536e23
jama <jamakazie@gmail.com>
2013-11-13 15:30:59 -0500

    EoD commit

---
+++ scripts/ex2.js
@@ -0,0 +1,36 @@
+var ex2 = {
+	validate : {
+		'ex-2-name' : {
+			required: true,
+			messageElement: '.message',
+			minLength: 5,
+		},
+		'ex-2-score' : {
+			rule: /^\d+$/,
+			messageElement: '.message',
+			ruleMessage : 'Input must be a positive integer'
+		},
+		'ex-2-hp' : {
+			rule: /^\d+$/,
+			messageElement: '.message',
+			ruleMessage: 'Input must be a positive integer'
+		},
+		'ex-2-mp' : {
+			rule: /^\d+$/,
+			messageElement: '.message',
+			ruleMessage: 'Input must be a positive integer'
+		},
+	},
+	groups : {
+		'group-1': {
+			container : 'ex-2-group-1',
+			errors: '#group-1',
+		},
+		'group-2': {
+			container: 'ex-2-group-2',
+			errors: '#group-2',
+		}
+	}
+};
+
+var jval2 = $('#form-2').jValidate(ex2);
